08c6282d409e8d5f13f3853a84551d14479ff052
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2021-06-11 21:13:26 +0100

    change crs init

--- shell-scripts/vancouver-crs.sh
+++ shell-scripts/vancouver-crs.sh
@@ -1,2 +1,2 @@
 OUT=results/vancouver_crs
-imageopt --loss LPIPSLoss --net vgg --invert --seed 1234 --width 300 --crs 500 --init-sigma2 1.0 --final-sigma2 1.0 --iters 500 --lr 0.01 --init-raster $OUT/init.png --final-raster $OUT/final.png --init-pdf $OUT/init.pdf --final-pdf $OUT/final.pdf --snapshots-path $OUT data/vancouver.jpg --snapshots-steps 100
+imageopt --loss LPIPSLoss --net vgg --invert --seed 1234 --width 300 --crs 500 --init-sigma2 1.0 --final-sigma2 1.0 --iters 500 --lr 0.01 --init-raster $OUT/init.png --final-raster $OUT/final.png --init-pdf $OUT/init.pdf --final-pdf $OUT/final.pdf --snapshots-path $OUT data/vancouver.jpg --snapshots-steps 100 --colour
