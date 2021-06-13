8218dea5d3b21f943d9a59ae02d0e4012ab0d140
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2021-06-13 10:51:04 +0100

    change crs init

--- dsketch/utils/pyxdrawing.py
+++ dsketch/utils/pyxdrawing.py
@@ -29,13 +29,9 @@ def draw_points(points: torch.Tensor, filename, size=1):
     c.writePDFfile(file=filename)
 
 
-def draw_points_lines(points: torch.Tensor, lines: torch.Tensor, filename, size=1, lw=1, pcols=None, lcols=None):
-    c = canvas.canvas()
-
+def draw_points_to_canvas(c, points: torch.Tensor, size=1, pcols=None):
     if pcols is not None:
         pcols = pcols.detach().cpu()
-    if lcols is not None:
-        lcols = lcols.detach().cpu()
 
     if points is not None:
         points = points.detach().cpu()
@@ -44,6 +40,11 @@ def draw_points_lines(points: torch.Tensor, lines: torch.Tensor, filename, size=
                 c.fill(path.circle(points[i, 1], -points[i, 0], size))
             else:
                 c.fill(path.circle(points[i, 1], -points[i, 0], size), [color.rgb(*pcols[i])])
+
+
+def draw_lines_to_canvas(c, lines: torch.Tensor, lw=1, lcols=None):
+    if lcols is not None:
+        lcols = lcols.detach().cpu()
 
     if lines is not None:
         lines = lines.detach().cpu()
@@ -56,4 +57,29 @@ def draw_points_lines(points: torch.Tensor, lines: torch.Tensor, filename, size=
                 c.stroke(path.line(lines[i, 0, 1], -lines[i, 0, 0], lines[i, 1, 1], -lines[i, 1, 0]),
                          [lw, style.linecap.round, color.rgb(*lcols[i])])
 
+
+def draw_points_lines(points: torch.Tensor, lines: torch.Tensor, filename, size=1, lw=1, pcols=None, lcols=None):
+    c = canvas.canvas()
+
+    draw_points_to_canvas(c, points, size, pcols)
+    draw_lines_to_canvas(c, lines, lw, lcols)
+
     c.writePDFfile(file=filename)
+
+
+def draw_crs_to_canvas(c, crs: torch.Tensor, lw=1, lcols=None):
+    # crs [n, nc, 2]; only draw nc>1<nc-1
+    if lcols is not None:
+        lcols = lcols.detach().cpu()
+
+    c.stroke
+    # if lines is not None:
+    #     lines = lines.detach().cpu()
+    #     lw = style.linewidth(lw)
+    #     for i in range(lines.shape[0]):
+    #         if lcols is None:
+    #             c.stroke(path.line(lines[i, 0, 1], -lines[i, 0, 0], lines[i, 1, 1], -lines[i, 1, 0]),
+    #                      [lw, style.linecap.round])
+    #         else:
+    #             c.stroke(path.line(lines[i, 0, 1], -lines[i, 0, 0], lines[i, 1, 1], -lines[i, 1, 0]),
+    #                      [lw, style.linecap.round, color.rgb(*lcols[i])])
